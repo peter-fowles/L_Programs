b03fdcc95d6e92eb81652dc78dd4afa07999e076
fowlinbasket <foul.in.basket@gmail.com>
2024-11-06 15:57:54 -0700

    Added demos, finished run() method

--- godel_numbers.py
+++ godel_numbers.py
@@ -1,5 +1,4 @@
 from sympy import prime
-from l_program import L_Program
 
 def natural_number(godel:list) -> int:
     if len(godel) < 1:
@@ -22,43 +21,3 @@ def factor_godel(x:int) -> list:
 
 def length_natural(x:int) -> int:
     return len(factor_godel(x))
-
-def encode_program(variables:list, labels:list, instructions:list) -> int:
-    num_p = []
-    for ins in instructions:
-        num_p.append(encode_instruction(variables, labels, ins))
-    return natural_number(num_p) - 1
-
-def encode_instruction(variables:list, labels:list, instruction:str) -> int:
-    labelled = instruction[0] == '['
-    instruction = instruction.split(' ')
-    a = 0
-    b = 0
-    c = 0
-    var = ''
-    if labelled:
-        a = labels.index(instruction[0][1:-1]) + 1
-        instruction = instruction[1:]
-    if instruction[0] == 'IF':
-        b = labels.index(instruction[-1]) + 3
-        var = instruction[1]
-    elif '+' in instruction:
-        b = 1
-        var = instruction[0]
-    elif '-' in instruction:
-        b = 2
-        var = instruction[0]
-    else:
-        b = 0
-        var = instruction[0]
-    c = variables.index(var) + 1
-    return compute_instruction(a, b, c)
-
-def compute_instruction(a, b, c):
-    right = 2**b * (2 * c + 1) - 1
-    return 2**a * (2 * right + 1) - 1
-
-def program_from_number(n:int) -> 
-
-if __name__ == '__main__':
-    print(factor_godel(2**21))
--- l_program.py
+++ l_program.py
@@ -1,8 +1,9 @@
 import re
-from godel_numbers import natural_number, length_natural, factor_godel
+from godel_numbers import natural_number, factor_godel
 
 LABELLED = re.compile(r'\[(\w+)\]')
 COMMAND = re.compile(r'IF (\w+) != 0 GOTO (\w+)|(\w+) <- (\w+)($|\n| ([+-]) (\d+))')
+IGNORE = re.compile(r'#.*')
 
 GROUPS = {
     'instruction':0,
@@ -23,35 +24,67 @@ class L_Program:
         if filename is not None:
             if lines is not None:
                 raise ValueError('Cannot have a filename and a list of lines')
-            with open(filename, 'r') as f:
-                for line in f:    
-                    ins = self.Instruction(line)
-                    if ins.variable not in self.variables:
-                        self.variables.append(ins.variable)
-                    if ins.label and ins.label not in self.labels:
-                        self.labels.append(ins.label)
-                        self.longest_label = max(len(ins.label), self.longest_label)
-                    self.instructions.append(ins)
+            lines = open(filename, 'r')
         elif lines is None:
             raise ValueError('No arguments were given')
-        else:
-            for line in lines:
-                    ins = self.Instruction(line)
-                    if ins.variable not in self.variables:
-                        self.variables.append(ins.variable)
-                    if ins.label and ins.label not in self.labels:
-                        self.labels.append(ins.label)
-                        self.longest_label = max(len(ins.label), self.longest_label)
-                    self.instructions.append(ins)
+        for line in lines:
+            if not line.strip() or IGNORE.match(line) is not None:
+                continue 
+            ins = self.Instruction(line)
+            if ins.variable not in self.variables:
+                self.variables.append(ins.variable)
+            if ins.label and ins.label not in self.labels:
+                self.labels.append(ins.label)
+                self.longest_label = max(len(ins.label), self.longest_label)
+            self.instructions.append(ins)
         self.program_length = len(self.instructions)
 
-    def __encode_instruction(self, ins):
+    def run(self, input_values:dict) -> int:
+        var_values = {'Y':0}
+        if 'Y' in input_values and input_values['Y'] != 0:
+            raise ValueError('The output variable \"Y\" cannot be initialized to anything other than 0')
+        for v in self.variables:
+            if v in input_values:
+                if not isinstance(input_values[v], int) or input_values[v] < 0:
+                    raise ValueError("All input variables must be natural numbers")
+                var_values[v] = input_values[v]
+            else:
+                var_values[v] = 0
+        done = False
+        curr_index = 0
+        while not done:
+            curr_instruction = self.instructions[curr_index]
+            if curr_instruction.goto is not None and var_values[curr_instruction.variable] != 0:
+                if curr_instruction.goto not in self.labels:
+                    done = True
+                else:
+                    for i in range(len(self.instructions)):
+                        if self.instructions[i].label == curr_instruction.goto:
+                            curr_index = i
+                            break
+            else: 
+                if curr_instruction.op is not None:
+                    if curr_instruction.op == '+':
+                        var_values[curr_instruction.variable] += 1
+                    elif var_values[curr_instruction.variable] > 0:
+                        var_values[curr_instruction.variable] -= 1
+                curr_index += 1
+        return var_values['Y']
+    
+    def source_number(self) -> int:
+        return self.program_number() + 1
+
+    def __encode_instruction(self, ins) -> int:
         if ins.label is not None:
             a = self.labels.index(ins.label) + 1
         else:
             a = 0
         if ins.groups[GROUPS['iflabel'] - 1] is not None:
-            b = self.labels.index(ins.groups[GROUPS['iflabel'] - 1]) + 3
+            iflabel = ins.groups[GROUPS['iflabel'] - 1]
+            if iflabel not in self.labels:
+                b = len(self.labels) + 2
+            else:
+                b = self.labels.index(iflabel) + 3
         elif ins.groups[GROUPS['op'] - 1] == '+':
             b = 1
         elif ins.groups[GROUPS['op'] - 1] == '-':
@@ -63,10 +96,10 @@ class L_Program:
         right = 2**b * (2 * c + 1) - 1
         return 2**a * (2 * right + 1) - 1
     
-    def encode(self):
+    def program_number(self) -> int:
         return natural_number(self.godel_number()) - 1
     
-    def godel_number(self):
+    def godel_number(self) -> list:
         num_p = []
         for ins in self.instructions:
             num_p.append(self.__encode_instruction(ins))
@@ -85,7 +118,6 @@ class L_Program:
 
     class Instruction:
         def __init__(self, line:str):
-            self.variable = None
             self.label = None
 
             labelled = LABELLED.search(line)
@@ -104,6 +136,8 @@ class L_Program:
                 var = cmd.group(GROUPS['v1'])
 
             self.variable = var
+            self.goto = cmd.group(GROUPS['iflabel'])
+            self.op = cmd.group(GROUPS['op'])
             self.text = line.strip()
             self.groups = cmd.groups()
 
@@ -112,10 +146,15 @@ class L_Program:
         
         def latex(self):
             s = f'[{self.label}] ' if self.label else ''
-            if self.groups[GROUPS['iflabel'] - 1]:
-                return s + f'IF {self.groups[GROUPS["ifvar"] - 1]} \\neq 0 GOTO {self.groups[GROUPS["iflabel"] - 1]}'
+            if self.goto:
+                return s + f'IF {self.variable} \\neq 0 GOTO {self.goto}'
             else:
-                return s + f'{self.groups[GROUPS["v1"] - 1]} \\gets {self.groups[GROUPS["v2"] - 1]}' + \
-                    (f' {self.groups[GROUPS["op"] - 1]} {self.groups[GROUPS["val"] - 1]}' if self.groups[GROUPS["op"] - 1] else '')
+                return s + f'{self.variable} \\gets {self.variable}' + \
+                    (f' {self.op} 1' if self.op else '')
 
 
+def program_from_number(program_num:int) -> L_Program:
+    source_num = program_num + 1
+    instruction_nums = factor_godel(source_num)
+    for ins_num in instruction_nums:
+        pass
