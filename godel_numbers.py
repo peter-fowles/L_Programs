4801c3900de55bf7840dae5fb9b0f090d5d70ec2
fowlinbasket <foul.in.basket@gmail.com>
2024-11-08 14:43:35 -0700

    Added split_natural_number

--- godel_numbers.py
+++ godel_numbers.py
@@ -21,3 +21,19 @@ def factor_godel(x:int) -> list:
 
 def length_natural(x:int) -> int:
     return len(factor_godel(x))
+
+def split_natural_number(z):
+    left = -1
+    right = -1
+    for x in range(z + 1):
+        for y in range(z + 1):
+            if 2**x * (2 * y + 1) - 1 == z:
+                if left < 0:
+                    left = x
+    for y in range(z + 1):
+        for x in range(z + 1):
+            if 2**x * (2 * y + 1) - 1 == z:
+                if right < 0:
+                    right = y
+
+    return left, right
